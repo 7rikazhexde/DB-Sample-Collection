fd23b0eb4833abde310d08fdaf6b67d0383c41a5
7rikaz_h785 <7rikaz.h785.stat2ltas41lcijad@gmail.com>
2023-10-16 00:49:11 +0900

    :recycle: Fixed code

--- sample/mongodb/mongodb_incert.py
+++ sample/mongodb/mongodb_incert.py
@@ -3,15 +3,26 @@ import csv
 
 import dateutil.parser
 from pymongo import MongoClient
+from tomlkit.toml_file import TOMLFile
+
+# Obtaining database connection information
+toml = TOMLFile("./config.toml")
+toml_data = toml.read()
+toml_get_data = toml_data.get("database-mac")
+
+HOST = toml_get_data["HOST"]
+PORT = str(toml_get_data["PORT"])
+USER = toml_get_data["USER"]
+PASSWORD = toml_get_data["PASSWORD"]
+DATABASE = toml_get_data["DATABASE"]
+COLLECTION = toml_get_data["COLLECTION"]
 
 # Connect to MongoDB
-# client = MongoClient("mongodb://localhost:27017/")
-# client = MongoClient("mongodb://admin:[pw]@localhost:27017/")
-client = MongoClient("mongodb://mac001:001mac@localhost:27017/")
+client = MongoClient(f"mongodb://{USER}:{PASSWORD}@{HOST}:{PORT}/")
 # Select DB
-db = client.ec_sol_db1
+db = client[f"{DATABASE}"]
 # Select a collection
-collection = db.ec_sol_pes_tbl
+collection = db[f"{COLLECTION}"]
 
 # Open CSV file
 with open("data.csv", "r") as f:
